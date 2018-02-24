b26e5a87b712eeb72c01003ae6374b7ef534b134
SummitStha <sumz.stha01@gmail.com>
2018-02-25 00:09:14 +0545

    simple blurring and smoothing operations added in color_filtering.py for removing noise

--- color_filtering.py
+++ color_filtering.py
@@ -15,9 +15,26 @@ while True:
 	mask = cv2.inRange(hsv, lower_red, upper_red)	# Create mask, which uses an "inRange" statement, for specific range. 
 	res = cv2.bitwise_and(frame, frame, mask=mask)	# Show color where there is the frame AND the mask. 
 
+	# Apply a simple smoothing, where we do a sort of averaging per block of pixels, here a 15 x 15 square
+	kernel = np.ones((15,15), np.float32)/225
+	smoothed = cv2.filter2D(res, -1, kernel)
+
+	# Gaussian Blur
+	blur = cv2.GaussianBlur(res, (15,15), 0)
+
+	# Median Blur
+	median = cv2.medianBlur(res, 15)
+
+	# Bilateral Filter
+	bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+
 	cv2.imshow('frame', frame)
 	cv2.imshow('mask', mask)
 	cv2.imshow('res', res)
+	cv2.imshow('smoothed', smoothed)
+	cv2.imshow('blur', blur)
+	cv2.imshow('median', median)
+	cv2.imshow('bilateral', bilateral)
 	
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
